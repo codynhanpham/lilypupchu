f1da124ab340ec1f726e95a0c1882003e461a3ce
Cody Nhan Pham <87593612+codynhanpham@users.noreply.github.com>
2022-01-21 02:51:19 -0600

    Update media.js

--- functions/models/media.js
+++ functions/models/media.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
-
 const mediaSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    source: String, // twitter or instagram
-    user: String, // source social media username
-    type: String, // image, video, gif
-    subject: String, // pending --> temmie, davinky, both, else.    else --> 
-    mediaID: String, // id leads to source post
-    mediaKey: String, // id leads to image (can be use in image url)
-    url: String, // image url
-    content: String, // content of social media post
-    created_at: String, // post uploaded date
-    meta: {
-        hash: String,
-        distance: Number
-    }
+  _id: mongoose.Schema.Types.ObjectId,
+  source: String, // twitter or instagram
+  user: String, // source social media username
+  type: String, // image, video, gif
+  subject: String, // pending --> temmie, davinky, both, else.    else --> 
+  mediaID: String, // id leads to source post
+  mediaKey: String, // id leads to image (can be use in image url)
+  url: String, // image url
+  content: String, // content of social media post
+  created_at: String, // post uploaded date
+  meta: {
+    hash: String,
+    distance: Number
+  }
 });
 
-module.exports = mongoose.model('Media', mediaSchema);
+module.exports = mongoose.model("Media", mediaSchema);
